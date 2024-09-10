9e928ba57a1ad9cddd6cd59bc3def7c4b35b388a
Mauricio_Vera <mcwanted123@gmail.com>
2024-09-10 18:53:39 -0500

    01

--- src/main/java/com/workconnect/model/entity/Company.java
+++ src/main/java/com/workconnect/model/entity/Company.java
@@ -1,4 +1,30 @@
 package com.workconnect.model.entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import java.util.List;
+
+@Data
+@Entity
+@Table(name = "company")
 public class Company {
-}
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id_user")
+    private int id;
+
+    @Column(name = "company_na", nullable = false, length = 50)
+    private String companyName;
+
+    @Column(name = "website", length = 50)
+    private String website;
+
+    @Column(name = "description", columnDefinition = "text")
+    private String description;
+
+    @Column(name = "country", length = 50)
+    private String country;
+
+    @OneToMany(mappedBy = "company", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Job> jobs;
