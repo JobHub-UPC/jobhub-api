e0a7956afa57aa93e03f5e3682561caf6c8997bc
Alecit03 <u20221c488@upc.edu.pe>
2024-09-13 09:24:22 -0500

    fix: correct userRepository

--- src/main/java/com/workconnect/repository/UserRepository.java
+++ src/main/java/com/workconnect/repository/UserRepository.java
@@ -1,10 +1,8 @@
 package com.workconnect.repository;
 
-public interface UserRepository {
 import com.workconnect.model.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User,Integer> {
-
 
 }
