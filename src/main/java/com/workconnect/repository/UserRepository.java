0e17d64dc5b9203ea61f82141343968b0f795121
Alecit03 <u20221c488@upc.edu.pe>
2024-09-12 20:30:49 -0500

    refactor:rebase

---
+++ src/main/java/com/workconnect/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.workconnect.repository;
+
+import com.workconnect.model.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User,Integer> {
+
+
+}
