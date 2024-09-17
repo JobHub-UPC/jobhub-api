ddbc8ea200f4d8ff24ff744ada4ece19552217d0
Jimena <jimena.qn26@gmail.com>
2024-09-17 17:42:09 -0500

    Refactor User service and implementation

--- src/main/java/com/workconnect/repository/UserRepository.java
+++ src/main/java/com/workconnect/repository/UserRepository.java
@@ -2,7 +2,10 @@ package com.workconnect.repository;
 
 import com.workconnect.model.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
+    boolean existByEmail(String email);
 
 }
