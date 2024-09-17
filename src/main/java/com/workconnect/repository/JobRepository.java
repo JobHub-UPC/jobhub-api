3e2470af5093d6d212c00bb326911a977f7b6cac
Jimena Quintana Noa <jimena.qn26@gmail.com>
2024-09-17 00:33:10 -0500

    Revert "feat(backend): CRUD implementation for user"

--- src/main/java/com/workconnect/repository/JobRepository.java
+++
@@ -1,7 +0,0 @@
-package com.workconnect.repository;
-
-import com.workconnect.model.entity.Job;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface JobRepository extends JpaRepository<Job, Integer> {
-}
