c1e31ad85b01b9253a35fad06ddcf2a88d957728
Jimena <jimena.qn26@gmail.com>
2024-11-06 17:04:53 -0500

    some changes

--- src/main/java/com/workconnect/config/WebSecurityConfig.java
+++ src/main/java/com/workconnect/config/WebSecurityConfig.java
@@ -57,6 +57,7 @@ public class WebSecurityConfig {
                         .requestMatchers(antMatcher("/auth/register/applicant")).permitAll()
                         .requestMatchers(antMatcher("/auth/register/company")).permitAll()
                         .requestMatchers(antMatcher("/mail/**")).permitAll()
+                        //.requestMatchers(antMatcher("/mail/reset/check/**")).permitAll()
                         .requestMatchers("/api/v1/swagger-ui/**", "/v3/api-docs/**", "/swagger-ui.html", "/swagger-ui/**", "/webjars/**").permitAll()
                         // TODO: Cualquier otra solicitud requiere autenticación (JWT u otra autenticación configurada)
                         .anyRequest().authenticated()
